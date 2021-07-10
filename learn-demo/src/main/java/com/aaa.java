c997151b9c65edd62562c2e087c07bc71cf25113
lanweibin <lanweibin0203@outlook.com>
2021-07-10 17:32:23 +0800

    Merge branch 'demo2'
    # Conflicts:
    #	learn-demo/src/main/java/com/aaa.java

--- learn-demo/src/main/java/com/aaa.java
+++ learn-demo/src/main/java/com/aaa.java
@@ -16,6 +16,7 @@ public class aaa {
     public static void main(String[] args) {
         System.out.println("1111111111111111111");
         System.out.println("222222222222222");
+        System.out.println(3333333);
     }
 
 
